70c4d7e5964603e9e31b8f62c5a19db2031f50ac
shrutanten <shrutanten.work@gmail.com>
2023-08-31 10:49:04 +0530

    Figure out the free currency

--- src/app/currency.service.ts
+++ src/app/currency.service.ts
@@ -6,6 +6,10 @@ import { Observable } from "rxjs";
 @Injectable({
   providedIn: "root",
 })
+
+/* 
+  EUR is the only currency available that can be used for conversion with a free account of the API
+*/
 export class CurrencyService {
   constructor(private httpClient: HttpClient) {}
 
@@ -16,12 +20,13 @@ export class CurrencyService {
 
   // function to return exchange rate of a base currency to specific currency
   getSpecificExchangeRate(from: string, to: string): Observable<any> {
+    // http://api.exchangeratesapi.io/latest?access_key=3fb9aaf97754b1dd99289bc13d2ba3e1&base=EUR&symbols=INR
     return this.httpClient.get(
       environment.API_URL + "&base=" + from + "&symbols=" + to
     );
   }
 
-  // function to return all exchnage rates for a specific base currency
+  // function to return all exchange rates for a specific base currency
   getAllExchangeRate(from: string): Observable<any> {
     return this.httpClient.get(environment.API_URL + "&base=" + from);
   }
