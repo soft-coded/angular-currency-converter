d353e640f4ed35cf2264f01511ba46fefaf6e861
shrutanten <shrutanten.work@gmail.com>
2023-08-31 16:33:45 +0530

    Add tests in service.spec file

--- test/currency.service.spec.ts
+++ test/currency.service.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from "@angular/core/testing";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
 
 import { CurrencyService } from "../src/app/currency.service";
+import { baseEurExchangeRates, usdToInr } from "./return-data";
+import { environment } from "src/environments/environment";
 
 describe("CurrencyService", () => {
   let service: CurrencyService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,6 +21,7 @@ describe("CurrencyService", () => {
 
   beforeEach(() => {
     service = TestBed.inject(CurrencyService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it("should be created", () => {
@@ -46,11 +53,35 @@ describe("CurrencyService", () => {
   it("getCurrencyList() should return all exchange rates for base EUR", () => {
     // Testcase to check whether function returns all exchange rates for base EUR
     // Use httpTestingController to create a mock backend to return a value(baseEurExchangeRates) from return-data.ts
+
+    service.getCurrencyList().subscribe((data) => {
+      expect(data).toEqual(baseEurExchangeRates);
+    });
+
+    const request = httpMock.expectOne({
+      method: "GET",
+      url: environment.API_URL,
+    });
+
+    request.flush(baseEurExchangeRates);
+    httpMock.verify();
   });
 
   it("getSpecificExchangeRate() should return exchange rate for particular base and currency", () => {
     // Testcase to check whether function send two currencies string('USD', 'INR') to backend
     // Use httpTestingController to create a mock backend to return a value(usdToInr) from return-data.ts
+
+    service.getSpecificExchangeRate("USD", "INR").subscribe((data) => {
+      expect(data).toEqual(usdToInr);
+    });
+
+    const request = httpMock.expectOne({
+      method: "GET",
+      url: environment.API_URL + "&base=USD&symbols=INR",
+    });
+
+    request.flush(usdToInr);
+    httpMock.verify();
   });
 
   it("getAllExchangeRate() should return all exchange rates for particular base", () => {
