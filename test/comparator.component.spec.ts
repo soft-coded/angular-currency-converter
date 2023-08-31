cd6f292e4f932b34f66df1ce5474a9a3082514b7
shrutanten <shrutanten.work@gmail.com>
2023-08-31 11:56:57 +0530

    Add tests

--- test/comparator.component.spec.ts
+++ test/comparator.component.spec.ts
@@ -1,16 +1,15 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 
-import { ComparatorComponent } from '../src/app/comparator/comparator.component';
+import { ComparatorComponent } from "../src/app/comparator/comparator.component";
 
-describe('ComparatorComponent', () => {
+describe("ComparatorComponent", () => {
   let component: ComparatorComponent;
   let fixture: ComponentFixture<ComparatorComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ ComparatorComponent ],
-    })
-    .compileComponents();
+      declarations: [ComparatorComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,17 +18,21 @@ describe('ComparatorComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     // Testcase to check component existence
+    expect(component).toBeTruthy("Could not create Comparator component");
   });
 
-  it('should have ngOnChnages()', () => {
+  it("should have ngOnChanges()", () => {
     // Testcase to check function existence
+    expect(component.ngOnChanges).toBeTruthy(
+      "Comparator component does not have ngOnChanges"
+    );
   });
 
-  it('ngOnChnages() should return exchangeRates', () => {
+  it("ngOnChanges() should return exchangeRates", () => {
     // Testcase to check whether the function returns exchange rates for a base currency
     // Use spyOn to give a value('baseUsdExchangeRates') from return-data.ts when a function of service is called
+    
   });
-
 });
